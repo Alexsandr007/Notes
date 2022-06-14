7aea49fb628ae5476d0754a1b0aec15b6add8d55
Alex_djangp_projects <aleksone_01@mail.ru>
2022-06-14 13:16:19 +0300

    Добавьте файлы проекта.

---
+++ Notes/Models/NotesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notes.Models
+{
+    public class NotesModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public User NoteUser { get; set; }
+        public string Text { get; set; }
+    }
+}
